bb0fbb362c8a056e73655c61f2ce4425be2990bb
ale_deulo <alejandro.deulofeu@towersoft.net>
2021-11-03 22:31:10 -0400

    first commit

---
+++ src/test/java/com/salesforce/smallstore/SmallstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesforce.smallstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
